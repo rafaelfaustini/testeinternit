c258fc69d936b7f5791bb08d3720c43aef969991
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-08-09 18:07:00 -0300

    Modelagem de banco

---
+++ Modelagem/Tabelas.sql
@@ -0,0 +1,23 @@
+create table assinante(
+  cpf varchar(14) Primary Key,
+  nome varchar(100),
+  email varchar(100),
+  senha varchar(255),
+  endereco varchar(150),
+  cidade varchar(50),
+  uf char(2)
+);
+
+create table noticia(
+  id int Primary Key AUTO_INCREMENT,
+  data date NOT NULL,
+  resumo text,
+  destaque boolean
+);
+
+create table imagens(
+  id int Primary Key AUTO_INCREMENT,
+  noticiaID int,
+  FOREIGN KEY (noticiaID) REFERENCES noticia(id),
+  caminho varchar(255)
+);
